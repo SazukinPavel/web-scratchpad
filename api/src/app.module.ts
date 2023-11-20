579ef102f3a5257459568f000911899b48ac8e5f
SazukinPavel <sazukinpavelal@gmail.com>
2023-11-21 00:59:21 +0400

    add gql error handling & refactor

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      formatError: (error: any) => {
+        const graphQLFormattedError = {
+          message:
+            error.extensions?.exception?.response?.message || error.message,
+          code:
+            error.extensions?.status ||
+            error.extensions?.code ||
+            'SERVER_ERROR',
+          name:
+            error.extensions?.originalError?.error ||
+            error.extensions?.exception?.name ||
+            error.name,
+        };
+        return graphQLFormattedError;
+      },
+    }),
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGO_DATABASE_ROOT_USERNAME}:${process.env.MONGO_DATABASE_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE_NAME}?authSource=admin`,
     ),
--- api/src/notes/notes.module.ts
+++ api/src/notes/notes.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { NoteResolver } from './notes.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Note, NoteSchema } from 'src/models/note.model';
@@ -8,12 +6,8 @@ import { NotesService } from './notes.service';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-    }),
     MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }]),
   ],
-  providers: [NoteResolver,NotesService],
+  providers: [NoteResolver, NotesService],
 })
 export class NotesModule {}
