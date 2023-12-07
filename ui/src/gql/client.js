003dce2d8731a87a107efdfae99a1f0f03e75a79
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-12-07 22:37:31 +0400

    fix gql server url

--- ui/src/gql/client.js
+++ ui/src/gql/client.js
@@ -1,6 +1,6 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 export default  new ApolloClient({
-    uri: 'http://localhost:3000/graphql',
+    uri: import.meta.env.VITE_API_URL,
     cache: new InMemoryCache(),
 });
