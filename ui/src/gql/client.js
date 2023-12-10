f3e6d4b93cdfbc6f0d69c011780458fe401eb0b6
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-12-11 00:41:27 +0400

    fix env api port;

--- ui/src/gql/client.js
+++ ui/src/gql/client.js
@@ -3,7 +3,7 @@ import {setContext} from '@apollo/client/link/context';
 import store from '../store/index.js';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3000/' + 'graphql',
+    uri: import.meta.env.VITE_API_URL + 'graphql',
 });
 
 const authLink = setContext((_, {headers}) => {
